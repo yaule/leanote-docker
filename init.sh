5bc3faf1dfb28cc6e61a4157d0927124fcaae891
lijixing <lijixing@papayamobile.com>
2017-09-06 10:04:37 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p /data/mongodb/conf && mkdir -p /data/mongodb/data
 cp /root/mongodb.conf /data/mongodb/conf/mongodb.conf
-source /etc/profile && mongod -f /data/mongodb/conf/mongodb.conf && mongorestore -h localhost -d leanote --dir /usr/local/leanote/mongodb_backup/leanote_install_data/
+source /etc/profile && mongod -f /data/mongodb/conf/mongodb.conf & ; mongorestore -h localhost -d leanote --dir /usr/local/leanote/mongodb_backup/leanote_install_data/
 
+hash=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-64};echo;); \
+    sed -i "s/app.secret=.*$/app.secret=$hash #/" /usr/local/leanote/conf/app.conf;
